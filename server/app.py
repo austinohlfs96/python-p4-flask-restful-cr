d9585b595915a30a91abd48f7d4e741985193b57
Austin Ohlfs <ohlfsam96@gmail.com>
2023-11-16 09:06:38 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -17,7 +17,117 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+    def get(self):
+        
+        response_dict = {
+            "message": "Welcome to the newsletter RESTful API",
+        }
+        response = make_response(
+            response_dict,
+            200
+        )
+        
+        return response
+
+   
+api.add_resource(Home, '/')
+
+class Newsletters(Resource):
+
+    def get(self):
+
+        response_dict_list = []
+        for n in  Newsletter.query.all():
+            response_dict_list.append(n.to_dict())
+            response = make_response(
+                response_dict_list,
+                200
+            )
+
+        return response
+    
+    def post(self):
+        try:
+            new_record = Newsletter(
+                title=request.form['title'],
+                body=request.form['body'],
+            )
+
+            db.session.add(new_record)
+            db.session.commit()
+
+            response_dict = new_record.to_dict()
+
+            return make_response(
+                response_dict,
+                201,
+            )
+            
+        except Exception as e:
+            return make_response({"error": str(e)}, 500)
+
+api.add_resource(Newsletters, '/newsletters')
+    
+class NewsletterByID(Resource):
+
+    def get(self, id):
+
+        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+
+        return response
+    
+    # def delete(self, id):
+        
+    #     db.session.delete(self)
+    #     db.session.commit()
+        
+    #     return "item deleted"
+    def delete(self, id):
+
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+    
+    def patch(self, id):
+
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+        
+    
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
+
+
+    
+
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
